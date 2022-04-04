1b819bba29c0e75608b2742796914741f1d26d9d
Jerome Bacatan <jbacatan@johnsonsberkshire.com>
2022-04-04 11:11:55 +0800

    added tailwind-css

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM  from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App"
-import Documents from "./routes/Documents";
+import Documents from "./routes/documents";
+import Login from "./routes/login";
+
+import "./index.css"
 
 const rootElement = document.getElementById('root')
 
 ReactDOM.render(
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<App />}>
+            <Route path="/" element={<Login />} />
+            <Route element={<App />}>
                 <Route path="/document" element={<Documents />} />
             </Route>
         </Routes>
