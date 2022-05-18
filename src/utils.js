aa8e94817e0947e1ae1395cc0d17a06170b2b16c
Jerome Bacatan <jbacatan@johnsonsberkshire.com>
2022-05-18 17:20:30 +0800

    minor changed on getDate()

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,16 @@
 const getDate = () => {
-    const date = new Date()
-    const month = date.getMonth()
-    const day = date.getDate()
-    const year = date.getFullYear()
-    
-    return `${day}/${month + 1}/${year}`
-}
+  const date = new Date();
+  const month = date.getMonth();
+  const day = date.getDate();
+  const year = date.getFullYear();
+  const hrs = date.getHours();
+  const mins = date.getMinutes();
+
+  return `${day}/${month + 1}/${year}  ${hrs}:${mins}`;
+};
 
 const removePrefixK = (string) => {
-    return string.at(0) === 'k' ? string.slice(2) : string
-}
+  return string.at(0) === "k" ? string.slice(2) : string;
+};
 
-export {
-    getDate,
-    removePrefixK,
-}
+export { getDate, removePrefixK };
