04198e2d8d5a32016661bec36d4bfe80b9a591ea
Jerome Bacatan <jbacatan@johnsonsberkshire.com>
2022-05-24 10:05:51 +0800

    changes due to prettier enable

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,268 +1,303 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom';
-import { useTable, useSortBy, useGlobalFilter, useAsyncDebounce, usePagination } from 'react-table'
-import FeatherIcon from 'feather-icons-react';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  useTable,
+  useSortBy,
+  useGlobalFilter,
+  useAsyncDebounce,
+  usePagination,
+} from "react-table";
+import FeatherIcon from "feather-icons-react";
 
-const classNames = (...classes) =>{
-    return classes.filter(Boolean).join(" ");
-}
+const classNames = (...classes) => {
+  return classes.filter(Boolean).join(" ");
+};
 
 const Button = ({ children, className, ...rest }) => {
-    return (
-        <button
-            type="button"
-            className={classNames(
-            "relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50",
-            className
-            )}
-            {...rest}
-        >
-            {children}
-        </button>
-    );
-}
-  
+  return (
+    <button
+      type="button"
+      className={classNames(
+        "relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50",
+        className
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
 const PageButton = ({ children, className, ...rest }) => {
-    return (
-        <button
-        type="button"
-        className={classNames(
-            "relative inline-flex items-center px-2 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50",
-            className
-        )}
-        {...rest}
-        >
-        {children}
-        </button>
-    );
-}
+  return (
+    <button
+      type="button"
+      className={classNames(
+        "relative inline-flex items-center px-2 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50",
+        className
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
 
 const GlobalFilter = ({
-    preGlobalFilteredRows,
-    globalFilter,
-    setGlobalFilter,
+  preGlobalFilteredRows,
+  globalFilter,
+  setGlobalFilter,
 }) => {
-    const count = preGlobalFilteredRows.length
-    const [value, setValue] = React.useState(globalFilter)
-    const onChange = useAsyncDebounce(value => {
-      setGlobalFilter(value || undefined)
-    }, 200)
-  
-    return (
-      <label className="flex gap-x-2 items-baseline">
-        <span className="text-gray-700">Search: </span>
-        <input
-          type="text"
-          class="mt-1 block sm:w-1/4 rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring-opacity-50"
-          value={value || ""}
-          onChange={e => {
-            setValue(e.target.value);
-            onChange(e.target.value);
-          }}
-          placeholder={`${count} records...`}
-        />
-      </label>
-    )
-}
+  const count = preGlobalFilteredRows.length;
+  const [value, setValue] = React.useState(globalFilter);
+  const onChange = useAsyncDebounce((value) => {
+    setGlobalFilter(value || undefined);
+  }, 200);
 
-const Pagination = (
-    {
-        canPreviousPage, 
-        canNextPage, 
-        pageOptions, 
-        pageCount,
-        gotoPage,
-        nextPage,
-        previousPage,
-        setPageSize,
-        state,
-    }
-) => {
-    return (
-        <div className="py-3 flex items-center justify-between">
-           <div className="flex-1 flex justify-between sm:hidden">
-                <Button onClick={() => previousPage()} disabled={!canPreviousPage}>Previous</Button>
-                <Button onClick={() => nextPage()} disabled={!canNextPage}>Next</Button>
-            </div>
-            <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
-                <div className="flex gap-x-2">
-                    <span className="text-sm text-gray-700">
-                    Page <span className="font-medium">{state.pageIndex + 1}</span> of <span className="font-medium">{pageOptions.length}</span>
-                    </span>
-                    <select
-                    value={state.pageSize}
-                    onChange={e => {
-                        setPageSize(Number(e.target.value))
-                    }}
-                    >
-                    {[5, 10, 20].map(pageSize => (
-                        <option key={pageSize} value={pageSize}>
-                        Show {pageSize}
-                        </option>
-                    ))}
-                    </select>
-                </div>
-                <div>
-                    <nav className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
-                    <PageButton
-                        className="rounded-l-md"
-                        onClick={() => gotoPage(0)}
-                        disabled={!canPreviousPage}
-                    >
-                        <span className="sr-only">First</span>
-                        <FeatherIcon icon="chevrons-left" className="h-5 w-5" aria-hidden="true" />
-                    </PageButton>
-                    <PageButton
-                        onClick={() => previousPage()}
-                        disabled={!canPreviousPage}
-                    >
-                        <span className="sr-only">Previous</span>
-                        <FeatherIcon icon="chevron-left" className="h-5 w-5" aria-hidden="true" />
-                    </PageButton>
-                    <PageButton
-                        onClick={() => nextPage()}
-                        disabled={!canNextPage
-                        }>
-                        <span className="sr-only">Next</span>
-                        <FeatherIcon icon="chevron-right" className="h-5 w-5" aria-hidden="true" />
-                    </PageButton>
-                    <PageButton
-                        className="rounded-r-md"
-                        onClick={() => gotoPage(pageCount - 1)}
-                        disabled={!canNextPage}
-                    >
-                        <span className="sr-only">Last</span>
-                        <FeatherIcon icon="chevrons-right" className="h-5 w-5" aria-hidden="true" />
-                    </PageButton>
-                    </nav>
-                </div>
-            </div>
+  return (
+    <label className="flex gap-x-2 items-baseline">
+      <span className="text-gray-700">Search: </span>
+      <input
+        type="text"
+        class="mt-1 block sm:w-1/4 rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring-opacity-50"
+        value={value || ""}
+        onChange={(e) => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
+        placeholder={`${count} records...`}
+      />
+    </label>
+  );
+};
+
+const Pagination = ({
+  canPreviousPage,
+  canNextPage,
+  pageOptions,
+  pageCount,
+  gotoPage,
+  nextPage,
+  previousPage,
+  setPageSize,
+  state,
+}) => {
+  return (
+    <div className="py-3 flex items-center justify-between">
+      <div className="flex-1 flex justify-between sm:hidden">
+        <Button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous
+        </Button>
+        <Button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </Button>
+      </div>
+      <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+        <div className="flex gap-x-2">
+          <span className="text-sm text-gray-700">
+            Page <span className="font-medium">{state.pageIndex + 1}</span> of{" "}
+            <span className="font-medium">{pageOptions.length}</span>
+          </span>
+          <select
+            value={state.pageSize}
+            onChange={(e) => {
+              setPageSize(Number(e.target.value));
+            }}
+          >
+            {[5, 10, 20].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </select>
         </div>
-    )
-}
+        <div>
+          <nav
+            className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
+            aria-label="Pagination"
+          >
+            <PageButton
+              className="rounded-l-md"
+              onClick={() => gotoPage(0)}
+              disabled={!canPreviousPage}
+            >
+              <span className="sr-only">First</span>
+              <FeatherIcon
+                icon="chevrons-left"
+                className="h-5 w-5"
+                aria-hidden="true"
+              />
+            </PageButton>
+            <PageButton
+              onClick={() => previousPage()}
+              disabled={!canPreviousPage}
+            >
+              <span className="sr-only">Previous</span>
+              <FeatherIcon
+                icon="chevron-left"
+                className="h-5 w-5"
+                aria-hidden="true"
+              />
+            </PageButton>
+            <PageButton onClick={() => nextPage()} disabled={!canNextPage}>
+              <span className="sr-only">Next</span>
+              <FeatherIcon
+                icon="chevron-right"
+                className="h-5 w-5"
+                aria-hidden="true"
+              />
+            </PageButton>
+            <PageButton
+              className="rounded-r-md"
+              onClick={() => gotoPage(pageCount - 1)}
+              disabled={!canNextPage}
+            >
+              <span className="sr-only">Last</span>
+              <FeatherIcon
+                icon="chevrons-right"
+                className="h-5 w-5"
+                aria-hidden="true"
+              />
+            </PageButton>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Table = ({ columns, data }) => {
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        page,
-        prepareRow,
-        state,
-        preGlobalFilteredRows,
-        setGlobalFilter,
-        canPreviousPage,
-        canNextPage,
-        pageOptions,
-        pageCount,
-        gotoPage,
-        nextPage,
-        previousPage,
-        setPageSize,
-      } = useTable(
-        {
-            columns,
-            data,
-            initialState: { pageIndex: 0 },
-        },
-        useGlobalFilter,
-        useSortBy,
-        usePagination,
-    )
-    const navigate = useNavigate()
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    prepareRow,
+    state,
+    preGlobalFilteredRows,
+    setGlobalFilter,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0 },
+    },
+    useGlobalFilter,
+    useSortBy,
+    usePagination
+  );
+  const navigate = useNavigate();
 
-    const paginationProps = { 
-        canPreviousPage, 
-        canNextPage, 
-        pageOptions, 
-        pageCount,
-        gotoPage,
-        nextPage,
-        previousPage,
-        setPageSize,
-        state,
-     }
-    
-    console.log('--page--')
-    console.log(page)
-    return (
-        <div className="mt-2 flex flex-col">
-            
-            {/* Search */}
-            <GlobalFilter 
-                preGlobalFilteredRows={preGlobalFilteredRows} 
-                globalFilter={state.globalFilter}
-                setGlobalFilter={setGlobalFilter} />
+  const paginationProps = {
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state,
+  };
 
-            <div className="overflow-x-auto  my-5">
-                <div className="py-2 align-middle inline-block min-w-full">
-                    <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                    <table {...getTableProps()} className="min-w-full divide-y divide-gray-200">
-                        <thead className="bg-gray-50">
-                        {headerGroups.map(headerGroup => (
-                            <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map(column => (
-                                <th
-                                scope="col"
-                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                {...column.getHeaderProps(column.getSortByToggleProps())}
-                                >
-                                {column.render('Header')}
-                                <span>
-                                    {column.isSorted
-                                    ? column.isSortedDesc
-                                        ? ' ▼'
-                                        : ' ▲'
-                                    : ''}
-                                </span>
-                                </th>
-                            ))}
-                            </tr>
-                        ))}
-                        </thead>
-                        <tbody
-                        {...getTableBodyProps()}
-                        className="bg-white divide-y divide-gray-200"
-                        >
-                        {page.map((row, i) => {  // new
-                            prepareRow(row)
-                            return (
-                            <tr {...row.getRowProps()} className="hover:bg-gray-50 cursor-pointer transition" 
-                                onClick={() => row.original.link ? navigate(row.original.link) : ''}>
+  return (
+    <div className="mt-2 flex flex-col">
+      {/* Search */}
+      <GlobalFilter
+        preGlobalFilteredRows={preGlobalFilteredRows}
+        globalFilter={state.globalFilter}
+        setGlobalFilter={setGlobalFilter}
+      />
 
-                                {row.cells.map(cell => {
-                                    const value = cell.value.length > 50 ? `${cell.value.slice(0, 50)}...` : cell.value
-                                return (
-                                    <td
-                                    {...cell.getCellProps()}
-                                    className="px-6 py-4 whitespace-nowrap"
-                                    >
-                                    {value}
-                                    </td>
-                                )
-                                })}
-                            </tr>
-                            )
-                        })}
-                        </tbody>
-                    </table>
-                    </div>
-                </div>
-            </div>
-            
-            {/* Pagination */}
-            <Pagination {...paginationProps}/>
-      </div>
-      
-    )
-}
-
-function ReactTable({columns, data}) {
-    return (
-        <div>
-            <Table columns={columns} data={data} />
+      <div className="overflow-x-auto  my-5">
+        <div className="py-2 align-middle inline-block min-w-full">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table
+              {...getTableProps()}
+              className="min-w-full divide-y divide-gray-200"
+            >
+              <thead className="bg-gray-50">
+                {headerGroups.map((headerGroup) => (
+                  <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map((column) => (
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        {...column.getHeaderProps(
+                          column.getSortByToggleProps()
+                        )}
+                      >
+                        {column.render("Header")}
+                        <span>
+                          {column.isSorted
+                            ? column.isSortedDesc
+                              ? " ▼"
+                              : " ▲"
+                            : ""}
+                        </span>
+                      </th>
+                    ))}
+                  </tr>
+                ))}
+              </thead>
+              <tbody
+                {...getTableBodyProps()}
+                className="bg-white divide-y divide-gray-200"
+              >
+                {page.map((row, i) => {
+                  // new
+                  prepareRow(row);
+                  return (
+                    <tr
+                      {...row.getRowProps()}
+                      className="hover:bg-gray-50 cursor-pointer transition"
+                      onClick={() =>
+                        row.original.link ? navigate(row.original.link) : ""
+                      }
+                    >
+                      {row.cells.map((cell) => {
+                        const value =
+                          cell.value.length > 50
+                            ? `${cell.value.slice(0, 50)}...`
+                            : cell.value;
+                        return (
+                          <td
+                            {...cell.getCellProps()}
+                            className="px-6 py-4 whitespace-nowrap"
+                          >
+                            {value}
+                          </td>
+                        );
+                      })}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
         </div>
-    )
+      </div>
+
+      {/* Pagination */}
+      <Pagination {...paginationProps} />
+    </div>
+  );
+};
+
+function ReactTable({ columns, data }) {
+  return (
+    <div>
+      <Table columns={columns} data={data} />
+    </div>
+  );
 }
 
-export default ReactTable
+export default ReactTable;
